d7bcad1b22d5eed870cd00cf8d62db05298dd2db
Eslamtamim <mimatmalxe@gamil.com>
2025-01-19 22:44:54 +0200

    get access_token from refresh_token

--- helpers/spotifty_helpers.ts
+++ helpers/spotifty_helpers.ts
@@ -1,5 +1,17 @@
+import * as fs from 'fs';
+import * as os from 'os';
+
 async function fetchWebApi(endpoint: string, method: string, body: any = null) {
-  const token = process.env['SPOTIFY_TOKEN'];
+  const tmpDir = os.tmpdir();
+  let token = '';
+  try {
+    token = fs.readFileSync(`${tmpDir}/token`, 'utf8').trim();
+  } catch (e) {
+    console.log('Token not found');
+    token = await getToken();
+    fs.writeFileSync(`${tmpDir}/token`, token);
+  }
+
   let payload: Record<string, any> = {
     headers: {
       Authorization: `Bearer ${token}`,
@@ -21,5 +33,33 @@ export async function UpdatePlaylist(playlist_id: string, uris: string) {
 
 export async function getTopTracks() {
   // Endpoint reference : https://developer.spotify.com/documentation/web-api/reference/get-users-top-artists-and-tracks
-  return (await fetchWebApi('v1/me/top/tracks?time_range=short_term&limit=10', 'GET')).items;
+  const res = await fetchWebApi('v1/me/top/tracks?time_range=short_term&limit=10', 'GET');
+  const items = res.items;
+  if (!items) {
+    console.log('No items found', res);
+  }
+  return items;
+}
+
+async function getToken(): Promise<string> {
+  const client_id = process.env['SPOTIFY_CLIENT_ID'];
+  const client_secret = process.env['SPOTIFY_CLIENT_SECRET'];
+  const refresh_token = process.env['SPOTIFY_REFRESH_TOKEN'];
+
+  const data = new URLSearchParams();
+  data.append('grant_type', 'refresh_token');
+  data.append('refresh_token', refresh_token);
+  data.append('client_id', client_id);
+  data.append('client_secret', client_secret);
+
+  const res = await fetch('https://accounts.spotify.com/api/token', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: data,
+  });
+
+  const json = await res.json();
+  return json.access_token;
 }
