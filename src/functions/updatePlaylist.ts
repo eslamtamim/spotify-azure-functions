cc7ea57b8ceac18433b13fe7054be82986b098bc
Eslamtamim <mimatmalxe@gamil.com>
2025-01-19 23:16:35 +0200

    no need to return and I need to throw an error if fail to show fail in app insights

--- src/functions/updatePlaylist.ts
+++ src/functions/updatePlaylist.ts
@@ -5,13 +5,12 @@ export async function updatePlaylist(myTimer: Timer, context: InvocationContext)
   const uris = (await getTopTracks())?.map((track: any) => track.uri).join(',');
   const playlist_id = process.env['SPOTIFY_PLAYLIST_ID'];
   if (!playlist_id || !uris) {
-    console.log('Playlist ID or URIs not found');
+    console.error('Playlist ID or URIs not found');
+    throw new Error('Playlist ID or URIs not found');
   }
   console.log('Playlist ID:', playlist_id);
   console.log('URIs:', uris);
-  const output = await UpdatePlaylist(playlist_id, uris);
-  console.log('Output:', output);
-  return output;
+  console.log('Output:', await UpdatePlaylist(playlist_id, uris));
 }
 
 // every one min
