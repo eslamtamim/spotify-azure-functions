2f984acb4c3eb91429f344019b78ff4695238c98
Eslamtamim <mimatmalxe@gamil.com>
2025-01-19 23:37:04 +0200

    return

--- src/functions/updatePlaylist.ts
+++ src/functions/updatePlaylist.ts
@@ -1,7 +1,7 @@
 import { app, InvocationContext, Timer } from '@azure/functions';
 import { getTopTracks, UpdatePlaylist } from '../../helpers/spotifty_helpers';
 
-export async function updatePlaylist(myTimer: Timer, context: InvocationContext): Promise<void> {
+export async function updatePlaylist(myTimer: Timer, context: InvocationContext) {
   const uris = (await getTopTracks())?.map((track: any) => track.uri).join(',');
   const playlist_id = process.env['SPOTIFY_PLAYLIST_ID'];
   if (!playlist_id || !uris) {
@@ -16,17 +16,19 @@ export async function updatePlaylist(myTimer: Timer, context: InvocationContext)
     throw new Error('Failed to update playlist');
   }
   console.info('Output:', output);
+  return output;
 }
 
-// every one min
-app.timer('updatePlaylist', {
-  schedule: '0 * * * * *',
-  handler: updatePlaylist,
-});
-
-// every one day
-/* app.timer('updatePlaylist', {
-  schedule: '0 0 * * *',
-  handler: updatePlaylist,
-});
- */
+if (process.env['DEV']) {
+  // every one min
+  app.timer('updatePlaylist', {
+    schedule: '0 * * * * *',
+    handler: updatePlaylist,
+  });
+} else {
+  // every one day
+  app.timer('updatePlaylist', {
+    schedule: '0 0 * * *',
+    handler: updatePlaylist,
+  });
+}
