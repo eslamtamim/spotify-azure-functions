1c76d837e7650f4d0d96cb403432cad319050a19
Eslam Tamim <mimatmalxe@gmail.com>
2025-01-21 11:45:11 +0200

    Update play.ts

--- src/functions/play.ts
+++ src/functions/play.ts
@@ -3,20 +3,20 @@ import { getCurrentPlaying, setCurrentPlaying } from '../helpers/spotifty_helper
 
 async function play(req: HttpRequest, _: InvocationContext): Promise<HttpResponse> {
   const id = req.query.get('id');
-  let res: Record<string, any> = {};
+  // let res: Record<string, any> = {};
   if (id) {
     console.log('req.url: ', req.url, 'id: ', id);
     await setCurrentPlaying(id);
-    res = { set_current: 'ok', ...res };
+    // res = { set_current: 'ok', ...res };
   }
   const playing = await getCurrentPlaying();
   const playing_status = playing.is_playing
     ? `${playing.item.name} - ${playing.item.artists.map((e: { name: any }) => e.name).join(', ')}`
     : 'not playing or paused, ping me a song :D';
-  res = { ...res, playing: playing_status };
+  // res = { ...res, playing: playing_status };
   return new HttpResponse({
     status: 200,
-    jsonBody: res,
+    body: playing_status,
   });
 }
 app.http('play', { route: 'play', methods: ['GET'], handler: play });
