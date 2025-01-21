eeacf8724e3d54fcc0fc4c9358e8a702476770ff
Eslam Tamim <mimatmalxe@gmail.com>
2025-01-21 12:41:19 +0200

    Update play.ts

--- src/functions/play.ts
+++ src/functions/play.ts
@@ -13,10 +13,46 @@ async function play(req: HttpRequest, _: InvocationContext): Promise<HttpRespons
   const playing_status = playing.is_playing
     ? `${playing.item.name} - ${playing.item.artists.map((e: { name: any }) => e.name).join(', ')}`
     : 'not playing or paused, ping me a song :D';
-  // res = { ...res, playing: playing_status };
+  const htmlResponse = `
+      <html>
+        <head>
+          <meta charset="UTF-8">
+          <style>
+            body {
+              display: flex;
+              justify-content: center;
+              align-items: center;
+              height: 100vh;
+              margin: 0;
+              font-family: Arial, sans-serif;
+              text-align: center;
+            }
+            h1 {
+              font-size: 2em;
+              font-weight: bold;
+            }
+            p {
+              font-size: 1.5em;
+              font-weight: bold;
+            }
+            img {
+              display: block;
+              margin: 0 auto;
+            }
+          </style>
+        </head>
+        <body>
+          <div>
+          ${playing.item.album.images[0]?.url && `<img src="${playing.item.album.images[0].url}" width="300" alt="album cover"/>`}
+            <p>${playing_status}</p>
+          </div>
+        </body>
+      </html>
+    `;
   return new HttpResponse({
     status: 200,
-    body: playing_status,
+    headers: { 'Content-Type': 'text/html' },
+    body: htmlResponse,
   });
 }
 app.http('play', { route: 'play', methods: ['GET'], handler: play });
